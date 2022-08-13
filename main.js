2f35218416f473c76d44f1daacbe1bb9d5799282
Hyppoprogramm <65360617+Hyppoprogramm@users.noreply.github.com>
2022-08-13 12:14:49 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 (class {
   static info = {
-      name: "title",
-      messages: {
-        "en": {
-          "title": "My mod",
-          "blocks.alert": "alert %1"
-        },
-        "ru": {
-          "title": "Мой мод",
-        }
+    name: "title",
+    messages: {
+      "en": {
+        "title": "My mod",
+        "blocks.alert": "alert %1"
+      },
+      "ru": {
+        "title": "Мой мод",
       }
-    };
-  }
+    }
+  };
   
   init() {
     this.scratch.addCommandBlock(this.scratch.localization.getMessage("blocks.alert", "[string]"), (message) => {
